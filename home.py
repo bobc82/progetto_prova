01ade4b71149083a723a7f0dd201608046e0f701
Robert Coniglio <coniglio.roberto82@gmail.com>
2023-09-27 09:47:27 +0200

    commenti

--- home.py
+++ home.py
@@ -3,15 +3,18 @@ from regression_predict import linear_housing, ransac_housing
 
 app = Flask(__name__)
 
+#nella home visualizzo il form contenente un input numerico che richiede il valore RM e un pulsante che fa redirect sulla route predict con una POST
 @app.route('/')
 def index():
    return render_template('form_predict.html')
 
+#Questa è l'url di "atterraggio" a seguito del submit all'interno del form precedente. La funzione predict legge il valore numerico rm dal form e poi fornisce due predizioni:
+#la prima è data dalla funzione linear_housing che fa uso del modello LinearRegressor e la seconda, ransac_housing, fa uso del modello RANSACRegressor. Le funzioni sono state
+#importate dal file regression_predict.py. Infine stampo a video i risultati visualizzando il template result.html a cui passo pred ed rpred. 
 @app.route('/predict', methods = ['POST'])
 def predict():
 	if request.method == 'POST':
 		room_med = float(request.form['rm'])
-		print(type(room_med))
 		lin_pred = linear_housing([[room_med]])
 		ransac_pred = ransac_housing([[room_med]])
 		return render_template("result.html", pred = lin_pred, rpred = ransac_pred)
--- regression_predict.py
+++ regression_predict.py
@@ -3,16 +3,15 @@ import re
 import os
 import numpy as np
 
+# carico modello LinearRegressor serielizzato e stampo la predizione da un nuovo valore in input
 def linear_housing(rm):
 	rlin = pickle.load(open(os.path.join('predictor', 'pkl_objects', 'lin_regressor.pkl'),'rb'))
 	y_pred = rlin.predict(rm)
 	return y_pred
 
+# carico modello RANSACRegressor serielizzato e stampo la predizione da un nuovo valore in input
 def ransac_housing(rm):
 	ran = pickle.load(open(os.path.join('predictor', 'pkl_objects', 'ransac_regressor.pkl'),'rb'))
 	y_pred_ransac = ran.predict(rm)
 	return y_pred_ransac
-
-print(linear_housing([[6]]))
-print(ransac_housing([[7]]))
 
