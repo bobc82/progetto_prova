478b1591afeac9c7436367fe905a99fd8f3a5fcb
robbyc82 <coniglio.roberto82@gmail.com>
2023-09-28 10:53:55 +0200

    controllo su valori negativi

--- regression_predict.py
+++ regression_predict.py
@@ -5,12 +5,18 @@ import os
 def linear_housing(rm):
 	rlin = pickle.load(open(os.path.join('predictor', 'pkl_objects', 'lin_regressor.pkl'),'rb'))
 	y_pred = rlin.predict(rm)
-	return y_pred
+	y_pred_f = 0
+	if y_pred[0] > 0:
+		y_pred_f = y_pred[0]
+	return y_pred_f
 
 # carico modello RANSACRegressor serielizzato e stampo la predizione da un nuovo valore in input
 def ransac_housing(rm):
 	ran = pickle.load(open(os.path.join('predictor', 'pkl_objects', 'ransac_regressor.pkl'),'rb'))
 	y_pred_ransac = ran.predict(rm)
-	return y_pred_ransac
+	y_pred_ransac_f = 0
+	if y_pred_ransac[0] > 0:
+		y_pred_ransac_f = y_pred_ransac[0]
+	return y_pred_ransac_f
 
 
