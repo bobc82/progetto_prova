aedc1350a45c93c8fa88cb47916dc1c6298b260e
robbyc82 <coniglio.roberto82@gmail.com>
2023-09-28 11:09:27 +0200

    up

--- housing.py
+++ housing.py
@@ -1,17 +1,15 @@
 import pandas as pd
 
-def visualizza_dataset():
+def carica_dataset():
 	df = pd.read_csv('housing.data.txt', sep='\s+')
 	df.columns=['CRIM','ZN','INDUS','CHAS','NOX','RM','AGE','DIS','RAD','TAX','PTRATIO','B','LSTAT','MEDV']
-	return df.head()
+	return df
 
 def visualizza_dataset_html():
-	df = pd.read_csv('housing.data.txt', sep='\s+')
-	df.columns=['CRIM','ZN','INDUS','CHAS','NOX','RM','AGE','DIS','RAD','TAX','PTRATIO','B','LSTAT','MEDV']
+	df = carica_dataset()
 	return df.head().to_html()
 
 def minimo_medv():
-	df = pd.read_csv('housing.data.txt', sep='\s+')
-	df.columns=['CRIM','ZN','INDUS','CHAS','NOX','RM','AGE','DIS','RAD','TAX','PTRATIO','B','LSTAT','MEDV']
+	df = carica_dataset()
 	y=df['MEDV'].values
 	return min(y)
